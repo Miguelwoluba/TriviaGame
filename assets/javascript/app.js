169ad50e12140de4c0e366e032d3cd058477bfc5
miguel mejia <mejiamiguel4@gmail.com>
2018-05-04 00:18:47 -0600

    some css pictures not working properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
         var questions = [
             {
-                q: "Is Real Madrid the best team in the world", a: "true"
+                q: "Is Real Madrid the best team in the world?", a: "true"
             },
             {
                 q: "Is the sky Blue?", a: "true"
@@ -21,29 +21,31 @@ $(document).ready(function () {
                 q: "Lightning never strikes in the same place twice", a: "false"
             },
             {
-                q: "If you cry in space the tears just stick to your face", a: "true"
+                q: "If you cry in space do the tears just stick to your face?", a: "true"
             },
             {
-                q: "If you cut an earthworm in half, both halves can regrow their body", a: "false"
+                q: "If you cut an earthworm in half, can both halves can regrow their body?", a: "false"
             },
             {
-                q: "Humans can distinguish between over a trillion different smells", a: "true"
+                q: "Can humans distinguish between over a trillion different smells?", a: "true"
             },
             {
-                q: "Adults have fewer bones than babies do", a: "true"
+                q: "Do adults have fewer bones than babies do?", a: "true"
             },
             {
-                q: "Napoleon Bonaparte was extremely short", a: "false"
+                q: "Was Napoleon Bonaparte was extremely short?", a: "false"
             },
             {
-                q: "Goldfish only have a memory of three seconds", a: "false"
+                q: "Do Goldfish only have a memory of three seconds?", a: "false"
             },
             {
-                q: "There are more cells of bacteria in your body than there are human cells", a: "true"
+                q: "Are there are more cells of bacteria in your body than there are human cells?", a: "true"
             },
 
         ];
 
+
+    var images = ["assets/images/real-madrid-win.jpg", "assets/images/sky.jpg", "assets/images/light.jpg", "assets/images/cryinginspace.jpg", "assets/images/earthworm.jpg", "assets/images/nose.jpg", "assets/images/humanBones.jpg", "assets/images/goldfish.jpg","assets/images/bacteria.jpg"];
 
         function run() {
             clearInterval(intervalId);
@@ -79,12 +81,13 @@ $(document).ready(function () {
             
         });
     
-
+        
 
         function renderQuestion() {
             // If there are still more questions, render the next one.
             if (questionIndex <= (questions.length - 1)) {
                 run();
+                
                 document.querySelector("#question").innerHTML = questions[questionIndex].q;
             }
             // If there aren't, render the end game screen.
@@ -116,12 +119,19 @@ $(document).ready(function () {
                 return;
             }
 
-            // Determine which key was pressed, make it lowercase, and set it to the userInput variable.
+            
             var clickValue = ($(this).attr("value"));
             
 
             // Only run this code if "t" or "f" were pressed.
             if (clickValue === "true" || clickValue === "false") {
+
+                //  img stuff
+                var questionImg = $("<img>");
+
+                questionImg.attr("src", images[questionIndex]);
+
+                $(".img-container").append(questionImg);
 
                 // If they guess the correct answer, increase and update score, alert them they got it right.
                 if (clickValue === questions[questionIndex].a) {
@@ -131,6 +141,7 @@ $(document).ready(function () {
                     renderQuestion();
                     timer = 10;
                     stop();
+                    
                 }
                 // If wrong, alert them they are wrong.
                 else {
@@ -143,8 +154,9 @@ $(document).ready(function () {
                 // Increment the questionIndex variable and call the renderQuestion function.
                 questionIndex++;
                 renderQuestion();
-                run();
+                // run();
                 // stop();
+                
 
             }
 
